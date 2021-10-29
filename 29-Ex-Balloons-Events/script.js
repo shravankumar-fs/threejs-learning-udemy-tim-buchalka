7f406362b4b4a035e13f3a118ddef3161649cb82
star dragon <59759431+stardragoncoder@users.noreply.github.com>
2021-10-29 09:47:58 +0530

    exercise events

--- 29-Ex-Balloons-Events/script.js
+++ 29-Ex-Balloons-Events/script.js
@@ -1,6 +1,5 @@
 let scene, renderer, camera;
 let list = [];
-let ADD = 0.2;
 let rayCast, mouse;
 
 function onMouseClick(e) {
@@ -9,35 +8,58 @@ function onMouseClick(e) {
 
   rayCast.setFromCamera(mouse, camera);
 
-  rayCast.intersectObjects(scene.children).forEach((obj) => {
-    obj.object.position.x = window.innerWidth * 100;
-    obj.object.position.y = window.innerHeight * 100;
-    console.log(obj.object.position);
+  let intersects = rayCast.intersectObjects(scene.children);
+  if (intersects.length == 0) return;
+  let hit = intersects[0].object;
+
+  list.forEach((item, idx) => {
+    if (item.object == hit) {
+      list.splice(idx, 1);
+      scene.remove(hit);
+    }
   });
+  // intersects.forEach((item) => {
+  //   list.forEach((balloon, idx) => {
+  //     if (balloon.object == item.object) {
+  //       list.splice(idx, 1);
+  //       scene.remove(balloon.object);
+  //     }
+  //   });
+  // });
 }
 function getRandom(start, end) {
   return start + Math.floor(Math.random() * (end - start));
 }
 
-function createBalloon(i) {
-  let m = new THREE.MeshPhongMaterial({
-    color: 0xffffff * Math.random(),
-    shininess: 200,
-    side: THREE.DoubleSide,
-  });
-  let g = new THREE.SphereGeometry(5, 30, 30);
-  let sphere = new THREE.Mesh(g, m);
-  sphere.position.x = -40 + i * 16;
-  sphere.position.y = -20;
-  sphere.position.z = getRandom(-10, 30);
-  list.push(sphere);
-  scene.add(sphere);
-}
-function createGeometry() {
-  for (let i = 0; i < 5; i++) {
-    createBalloon(i);
+class Balloon {
+  constructor() {
+    console.log("Hellow");
+    let m = new THREE.MeshPhongMaterial({
+      color: 0xffffff * Math.random(),
+      shininess: 200,
+      side: THREE.DoubleSide,
+    });
+    let g = new THREE.SphereGeometry(3, 30, 30);
+    let sphere = new THREE.Mesh(g, m);
+    sphere.position.x = getRandom(-40, 40);
+    sphere.position.y = -20;
+    sphere.position.z = getRandom(-10, 30);
+    this.object = sphere;
+    console.log(this.object);
+    scene.add(sphere);
+
+    this.ADD = getRandom(0.05, 0.15);
+    this.over = false;
+    this.top = 50;
+  }
+  advance() {
+    this.object.position.y += this.ADD;
+    if (this.object.position.y > this.top) {
+      this.over = true;
+    }
   }
 }
+
 let init = () => {
   //create scene
   scene = new THREE.Scene();
@@ -63,6 +85,7 @@ let init = () => {
   mouse = new THREE.Vector2();
   mouse.set(-1, -1);
   // createGeometry();
+  list.push(new Balloon());
   //create renderer
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -71,10 +94,18 @@ let init = () => {
   document.addEventListener("click", onMouseClick);
 };
 let mainLoop = () => {
-  list.forEach((sphere) => {
-    sphere.position.y += ADD;
+  if (Math.random() < 0.05) {
+    list.push(new Balloon());
+  }
+
+  list.forEach((balloon, idx) => {
+    balloon.advance();
+    if (balloon.over) {
+      list.splice(idx, 1);
+      scene.remove(balloon);
+    }
   });
-  if (Math.random() < 0.008) createGeometry();
+
   renderer.render(scene, camera);
 
   requestAnimationFrame(mainLoop);
